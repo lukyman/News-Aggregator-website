10d479fef43952746ffa59e956918bb8e3a67b5a
Abdulmanan <a.manan59@gmail.com>
2017-05-02 21:46:48 +0300

    title change

--- views/footer/template.marko.js
+++ views/footer/template.marko.js
@@ -6,7 +6,7 @@ var marko_template = module.exports = require("marko/html").t(__filename);
 function render(input, out) {
   var data = input;
 
-  out.w("<footer class=\"page-footer\"><div class=\"footer-copyright\"><div class=\"container\"><div class=\"row\"><div class=\"col l3 col m12 col s12\">&copy; Copyright 2017 Nipashe Media</div><div class=\"col l3 col m12 col s12\"><div class=\"social-icon\"><a href=\"https://www.facebook.com/nipashemedia/\"><i class=\"fa fa-facebook\"></i></a><a href=\"https://twitter.com/NipasheMedia\"><i class=\"fa fa-twitter\"></i></a></div></div><div class=\"col l6 col m12 col s12\"><div class=\"footer-menu\"><ul><li><a href=\"/privacy-policy\">Privacy</a></li><li><a href=\"/terms-of-use\">Terms Of Use</a></li><li><a href=\"/about-us\">About Us</a></li><li><a href=\"/contact-us\">Contact us</a></li></ul></div></div></div></div></div></footer>");
+  out.w("<footer class=\"page-footer\"><div class=\"footer-copyright\"><div class=\"container\"><div class=\"row\"><div class=\"col l3 col m12 col s12\">&copy; Copyright 2017 Nipashe Media</div><div class=\"col l3 col m12 col s12\"><div class=\"social-icon\"><a href=\"https://www.facebook.com/nipashemedia/\"><i class=\"fa fa-facebook\"></i></a><a href=\"https://twitter.com/NipasheMedia\"><i class=\"fa fa-twitter\"></i></a><div class=\"android-badge\"><a href=\"\"> <img src=\"../public/images/android_badge.png\" alt=\"logo\"></a></div></div></div><div class=\"col l6 col m12 col s12\"><div class=\"footer-menu\"><ul><li><a href=\"/privacy-policy\">Privacy</a></li><li><a href=\"/terms-of-use\">Terms Of Use</a></li><li><a href=\"/about-us\">About Us</a></li><li><a href=\"/contact-us\">Contact us</a></li></ul></div></div></div></div></div></footer>");
 }
 
 marko_template._ = render;
