36d1e7de4b07aa10fc024531eae9a5e23f550d08
Abdulmanan <a.manan59@gmail.com>
2017-05-03 13:13:53 +0300

    added bing crawler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
 //var bodyParser = require('body-parser');
+//var xml = require("xml");
 var sm = require('sitemap');
 var robots = require('robots.txt');
 
